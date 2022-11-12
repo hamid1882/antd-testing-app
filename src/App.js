a61d30dec4183340380229f47e789b259e7c392b
hamid1882 <hussainhafeez1100@gmail.com>
2022-11-12 18:29:23 +0530

    added test cases

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import TodoList from './components/TodoList/TodoList';
 import './App.css';
+import { useState } from 'react';
+
+const todoListData = [
+  {
+    name: "Have Dinner",
+    id: 1,
+    done: false
+  }
+]
 
 function App() {
+  const [todoList, setTodoList] = useState(todoListData);
+
+  const AddTodo = () => {
+    const newObj = {
+      name: "empty",
+      id: todoList ? todoList[todoList.length - 1].id : 1,
+      done: false
+    }
+
+    setTodoList((prevTodo) => prevTodo.push(newObj));
+  }
+
+
   return (
     <div className="App">
-      <TodoList />
+      <TodoList
+        todoList={todoList}
+        handleTodoList={AddTodo}
+      />
     </div>
   );
 }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,8 +1,8 @@
-import { Button, Card } from 'antd';
+import { Button, Card, Space } from 'antd';
 import React, { useState } from 'react';
 import 'antd/dist/antd.css';
 
-function TodoList() {
+function TodoList({ todoList, handleTodoList }) {
   const [isShowTodo, setIsShowTodo] = useState(false);
 
 
@@ -19,17 +19,22 @@ function TodoList() {
       </Button>
       {
         isShowTodo
-          ? <Card data-testid="items-container">
-            <Card.Grid style={{ width: "10%" }}>1.</Card.Grid>
-            <Card.Grid style={{ width: "75%" }}>Item</Card.Grid>
-            <Card.Grid style={{ width: "15%" }}><Button type="primary">Done</Button></Card.Grid>
+          ? <Card>
+            <Space style={{ display: "flex", flexDirection: "column" }}>
+              <Card data-testid="items-container">
+                <Card.Grid style={{ width: "10%" }}>1.</Card.Grid>
+                <Card.Grid style={{ width: "75%" }}>Item</Card.Grid>
+                <Card.Grid style={{ width: "15%" }}><Button type="primary">Done</Button></Card.Grid>
+              </Card>
+              <Button type="primary">Add A New Todo</Button>
+            </Space>
           </Card>
           :
           <Card data-testid="no-todo-text">
             <h2>Click on Generate to get Todo's</h2>
           </Card>
       }
-    </div>
+    </div >
   )
 }
 
